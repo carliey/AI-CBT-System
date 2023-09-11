8a23e7c16f1ca6058ef35bd73e2c26a9455d0fc4
carliey <mdcarliey@gmail.com>
2023-09-11 12:52:17 +0100

    unpublished tests page

--- src/pages/test/Unpublished.tsx
+++ src/pages/test/Unpublished.tsx
@@ -1,5 +1,48 @@
+import { Button, Paper, Stack, Typography } from "@mui/material";
+import { Test } from "../../types/test";
+import { useNavigate } from "react-router-dom";
+import { pendingTests } from "../../data/tests";
+
 const Unpublished = () => {
-  return <div>Unpublished</div>;
+  const navigate = useNavigate();
+
+  const handlePublish = (e: any) => {
+    e.stopPropagation();
+    console.log("handle unpublish");
+  };
+  return (
+    <div>
+      {pendingTests.map((test: Test, index) => (
+        <Paper key={index} elevation={3} sx={{ my: 3, p: 2 }}>
+          <Stack
+            direction="row"
+            spacing={1}
+            sx={{ cursor: "pointer" }}
+            alignItems={"center"}
+            onClick={() => {
+              navigate("published", {
+                state: test,
+                preventScrollReset: true,
+              });
+            }}
+          >
+            <Typography variant="h5" flex={1} component="h2">
+              {test.title} {index === 0}
+            </Typography>
+            <Typography variant="subtitle1" flex={1}>
+              Participants: {test.participants?.length}
+            </Typography>
+            <Typography variant="subtitle1" sx={{ width: "220px" }}>
+              Date: {new Date(test.datetime).toLocaleString()}
+            </Typography>
+            <Button variant="contained" onClick={handlePublish}>
+              Publish
+            </Button>
+          </Stack>
+        </Paper>
+      ))}
+    </div>
+  );
 };
 
 export default Unpublished;
